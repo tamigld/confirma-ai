d7b767d87e12e8ebd564393334c542fad26dd00a
Thamires Galdino <tgalves@rd.com.br>
2025-03-13 16:58:40 -0300

    repsonsividade

--- src/pages/Invite.tsx
+++ src/pages/Invite.tsx
@@ -1,93 +1,88 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../assets/images/data.png';
-import imgLeft from '../assets/images/img-left.png';
-import imgRight from '../assets/images/img-right.png';
+import fundo from '../assets/images/img-center.png';
 import nomeNoivos from '../assets/images/nomes-noivos.png';
 import Button from '../components/Button';
-import Header from "../components/Header";
 import ModalConfirm from '../components/modal/ModalConfirm';
 import ModalInfo from '../components/modal/ModalInfo';
 import { useGuests } from '../hooks/useGuests';
 import { Guest } from '../hooks/useGuestsStore';
 import handleConfettiClick from '../utils/confetti';
 
-export default function Invite(){
+export default function Invite() {
   const { id } = useParams();
-  const { findGuestById, editGuestById } = useGuests()
-  const [guest, setGuest] = useState<Guest>({} as Guest)
+  const { findGuestById, editGuestById } = useGuests();
+  const [guest, setGuest] = useState<Guest>({} as Guest);
   const [open, setOpen] = useState<boolean>(false);
   const [openInfo, setOpenInfo] = useState<boolean>(false);
   const [responseType, setResponseType] = useState<'happy' | 'sad'>('happy');
 
   const findGuest = async () => {
     const response = await findGuestById(id);
-    setGuest(response.data)
-  }
+    setGuest(response.data);
+  };
 
   const confirm = async (status: any) => {
     setResponseType(status === 'confirmado' ? 'happy' : 'sad');
-    await editGuestById(
-      id,
-      { confirmado: status }
-    );
+    await editGuestById(id, { confirmado: status });
     setOpen(true);
     status === 'confirmado' && handleConfettiClick();
-  }
+  };
 
   const openModal = () => {
-    setOpenInfo(true)
-  }
+    setOpenInfo(true);
+  };
 
   useEffect(() => {
-    findGuest()
-  }, [])
+    findGuest();
+  }, []);
 
   return (
-    <section className='w-full h-screen bg-marfim'>
-      <Header className='fixed bg-marfim' />
-      <div className='h-full flex justify-between'>
-        <div
-          className='w-[15%] bg-cover'
-          style={{ backgroundImage: `url(${imgLeft})` }}
-        />
-        <div className='w-[45%] flex flex-col justify-center items-center gap-6'>
-          <img src={nomeNoivos} className='w-60' />
-          <p className='font-montserrat text-texto text-center'>
-            Olá, {guest.nome}! <br /> <br />
-            Estamos muito felizes em convidá-lo para comemorar com a gente no
-            nosso almoço após o casamento civil!
-          </p>
-          <img src={data} className='w-64' />
-          <p className='font-montserrat text-texto font-semibold text-center'>
-            Por favor, confirme sua presença abaixo para nos ajudar a organizar
-            melhor o evento e reservar seu lugar.
-          </p>
-          <div className='flex flex-col gap-2 items-center'>
-            <Button
-              label='Confirmar presença 😍'
-              colorType='dourado'
-              value='confirmado'
-              onClick={(e) => confirm(e.target.value)}
+    <section className='w-full bg-marfim'>
+      <div
+        className='h-screen bg-cover flex justify-center items-center'
+        style={{ backgroundImage: `url(${fundo})` }}
+      >
+        <div className='w-full flex justify-center items-center'>
+          <div className='bg-marfim rounded-4xl lg:w-2/4 flex flex-col justify-center items-center gap-6 px-16 py-8'>
+            <img
+              src={nomeNoivos}
+              className='w-40 sm:w-50'
+              alt='Nome dos noivos'
             />
-            <Button
-              label='Não irei 😢'
-              value='nao'
-              colorType='cinza'
-              onClick={(e) => confirm(e.target.value)}
-            />
+            <p className='font-montserrat text-texto text-center text-sm sm:text-lg lg:text-xl'>
+              Olá, {guest.nome}! <br /> <br />
+              Estamos muito felizes em convidá-lo para comemorar com a gente no
+              nosso almoço após o casamento civil!
+            </p>
+            <img src={data} className='w-48 sm:w-64' alt='Data do evento' />
+            <p className='font-montserrat text-texto font-semibold text-center text-sm sm:text-base lg:text-lg'>
+              Por favor, confirme sua presença abaixo para nos ajudar a
+              organizar melhor o evento e reservar seu lugar.
+            </p>
+            <div className='flex flex-col gap-4 items-center'>
+              <Button
+                label='Confirmar presença 😍'
+                colorType='dourado'
+                value='confirmado'
+                onClick={(e) => confirm(e.target.value)}
+              />
+              <Button
+                label='Não irei 😢'
+                value='nao'
+                colorType='cinza'
+                onClick={(e) => confirm(e.target.value)}
+              />
+            </div>
+            <a
+              className='cursor-pointer underline text-texto text-sm sm:text-base lg:text-lg'
+              onClick={openModal}
+            >
+              Informações sobre o almoço
+            </a>
           </div>
-          <a
-            className='cursor-pointer underline text-texto'
-            onClick={openModal}
-          >
-            Informações sobre o almoço
-          </a>
         </div>
-        <div
-          className='w-[15%]'
-          style={{ backgroundImage: `url(${imgRight})` }}
-        />
       </div>
       <ModalConfirm
         data={guest}
@@ -95,10 +90,8 @@ export default function Invite(){
         onClose={() => setOpen(false)}
         type={responseType}
       />
-      <ModalInfo
-        isOpen={openInfo}
-        onClose={() => setOpenInfo(false)}
-      />
+      <ModalInfo isOpen={openInfo} onClose={() => setOpenInfo(false)} />
     </section>
   );
 }
+
